4dec312f26c60ea646004c3827b4ba034ec9896b
kkeonwoo <kunwoo1324@naver.com>
2024-11-18 01:07:24 +0900

    alf - sc01 scroll

--- enterpriseBlockChain/assets/js/scroll.js
+++ enterpriseBlockChain/assets/js/scroll.js
@@ -1,15 +1,23 @@
 const text = document.querySelectorAll('.txt-box');
+const video = document.querySelector('#mainVideo');
+
 const textTl = gsap.timeline({
     scrollTrigger: {
         trigger: '.sc-01',
         start: 'top top',
-        end: 'bottom top',
+        end: '+=3000 bottom',
         pin: true,
         pinSpacing: true,
-        scrub: true,
+        scrub: 1,
     }
 })
-
+.to(CSSRulePlugin.getRule(".sc-01::before"), { duration: 1, cssRule: { opacity: 0.5 } })
 text.forEach((txt, idx) => {
-    textTl.from(txt, { autoAlpha: 0, y: 100 })
+    textTl.from(txt, { 
+        opacity: 0,
+        onComplete: () => idx === 0 && $('#header').addClass('on'),
+    })
+    .to(txt, { 
+        opacity: 0,
+    })
 })
