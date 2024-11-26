208d74b4adf8444a916d1ee54eb71acfa9c5a5e1
DESKTOP-JT5CO9U\ADUP <kunwoo1324@naver.com>
2024-11-26 18:20:43 +0900

    alf - thickPickle intro

--- thickPickle/assets/js/common.js
+++ thickPickle/assets/js/common.js
@@ -1,6 +1,19 @@
 Enterprise = {
     init: function () {
+        this.introAni();
     },
+    introAni: function() {
+        const introIcon = $('.intro .ico-loader');
+        $('#wrap').imagesLoaded()
+        .done( function( instance ) {
+            const introTl = gsap.timeline()
+            .to(introIcon, { opacity: 0})
+            .to('.intro', { yPercent: -100})
+        })
+        .progress( function() {
+            gsap.from(introIcon, { opacity: 0, duration: 1, repeat: -1, yoyo: true})
+        });
+    }
 }
 
 $(() => {
