6641e915eedb1da577859eb57f8faa77de29b831
kkeonwoo <kunwoo1324@naver.com>
2024-11-26 23:23:26 +0900

    alf - enterPrise aria

--- enterpriseBlockChain/assets/js/common.js
+++ enterpriseBlockChain/assets/js/common.js
@@ -6,22 +6,32 @@ Enterprise = {
     },
     handleLang: function () {
         const navItem = $('#header .lang')
-        const navLink = navItem.find('.link');
+        const navLink = $('#header .lang > .link');
         const btnLang = navItem.find('li');
-
         navLink.on('click', function (e) {
             e.preventDefault();
             $(this).siblings().toggleClass('open');
+
+            if ($(this).siblings().hasClass('open')) {
+                $(this).attr('aria-expanded', true);
+            } else {
+                $(this).attr('aria-expanded', false);
+            }
         })
 
         btnLang.on('click', function (e) {
             e.preventDefault();
             $(this).addClass('active').siblings().removeClass('active');
+            if ($(this).hasClass('active')) {
+                $(this).find('.link').attr('aria-selected', true);
+                $(this).siblings().find('.link').attr('aria-selected', false);
+            }
         })
 
         $(window).on('click', function(e) {
             if (!$(e.target).closest(navItem).length) {
                 navLink.siblings().removeClass('open')
+                navLink.attr('aria-expanded', false);
             }
         })
     },
