4f26249a36c9bce5239d696a302352f971ae047a
DESKTOP-JT5CO9U\ADUP <kunwoo1324@naver.com>
2024-11-27 11:23:07 +0900

    alf - pickle

--- thickPickle/assets/js/common.js
+++ thickPickle/assets/js/common.js
@@ -1,6 +1,7 @@
 Enterprise = {
     init: function () {
         this.introAni();
+        this.setVimeo();
     },
     introAni: function() {
         const introIcon = $('.intro .ico-loader');
@@ -13,6 +14,16 @@ Enterprise = {
         .progress( function() {
             gsap.from(introIcon, { opacity: 0, duration: 1, repeat: -1, yoyo: true})
         });
+    },
+    setVimeo: function() {
+        // const vimeoPlayer = new Vimeo.Player('.vimeo-player', {
+        //     id: 122963,
+        //     width: 1920,
+        //     loop: true,
+        //     autoplay: true,
+        //     muted: true,
+        //     controls: false,
+        // });
     }
 }
 
