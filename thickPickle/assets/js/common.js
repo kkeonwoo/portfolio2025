b1b0f49caae7dc6798d30045f1bda5972fd66705
kkeonwoo <kunwoo1324@naver.com>
2024-12-02 01:25:23 +0900

    alf - pickle sc-info

--- thickPickle/assets/js/common.js
+++ thickPickle/assets/js/common.js
@@ -2,17 +2,20 @@ Enterprise = {
     init: function () {
         this.introAni();
         this.scrollHdrAni();
+        this.handleCursor();
     },
     introAni: function() {
         const introIcon = $('.intro .ico-loader');
         $('#wrap').imagesLoaded()
         .done( function( instance ) {
-            const heroTitle = gsap.utils.toArray('.sc-hero .title');
+            const heroTitle = $('.sc-hero .title');
+
+            gsap.killTweensOf(introIcon);
 
             const introTl = gsap.timeline()
             .to(introIcon, { opacity: 0})
             .to('.intro', { yPercent: -100})
-            .from(heroTitle, { autoAlpha: 0, y: 0, rotateX: -90, stagger: { each: 0.2, ease: 'power3.in', delay: 0.1}})
+            .from(heroTitle, { autoAlpha: 0, yPercent: 100, rotateX: -90, scaleX: 0.75, duration: 1, delay: 0.1, stagger: { each: 0.1, ease: 'power4.in'}})
         })
         .progress( function() {
             gsap.from(introIcon, { opacity: 0, duration: 1, repeat: -1, yoyo: true})
@@ -37,6 +40,20 @@ Enterprise = {
             lastScroll = curr;
         });
     },
+    handleCursor: function() {
+        // quickTo: 성능최적화 유틸리티
+        // transform: 순서대로 적용되는 변환 함수들의 조합.
+        const cursor = $('.cursor');
+        let xTo = gsap.quickTo(cursor, 'x', { duration: 0.4, ease: 'back(3)'})
+        let yTo = gsap.quickTo(cursor, 'y', { duration: 0.4, ease: 'back(3)'})
+
+        gsap.set(cursor, {xPercent: -50, yPercent: -50});
+
+        $(window).on('mousemove', function({ clientX: x, clientY: y}) {
+            xTo(x);
+            yTo(y);
+        })
+    }
 }
 
 $(() => {
--- thickPickle/assets/js/scroll.js
+++ thickPickle/assets/js/scroll.js
@@ -1,20 +1,20 @@
+gsap.registerPlugin(ScrollTrigger);
 gsap.defaults({ease: 'none'});
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext("2d");
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext("2d");
 
 const frameCount = 446;
+const videoSection = {
+    frame: 0
+}
 const currFrame = (idx) => {
     return `../../assets/images/seq/${idx.toString().padStart(4, '0')}.webp`;
 }
 
-const videoSection = {
-    frame: 0
-}
-
 const images = Array(frameCount).fill(null).map((_, idx) => {
     const img = new Image();
-    img.src = currFrame(idx);
+    img.src = currFrame(idx + 1);
     return img;
 })
 
@@ -24,17 +24,86 @@ const tl = gsap.timeline()
 ScrollTrigger.create({
     trigger: '.sc-character',
     start: 'top top',
-    end: '+=3000',
+    end: 'bottom center',
     animation: tl,
-    pin: true,
-    scrub: true,
-    onUpdate: () => {
-        render();
+    scrub: 0,
+    onUpdate: ({ progress }) => {
+        // render();
+        gsap.set('.sc-character .img', { attr: { src: images[videoSection.frame].src}})
+
+        if ( videoSection.frame > 30 && videoSection.frame < 110) {
+            $('.sc-character .txt1').addClass('ani-translate')
+        } else {
+            $('.sc-character .txt1').removeClass('ani-translate')
+        }
+
+        if ( videoSection.frame > 200 && videoSection.frame < 265) {
+            $('.sc-character .txt2').addClass('ani-translate')
+        } else {
+            $('.sc-character .txt2').removeClass('ani-translate')
+        }
+
+        if ( videoSection.frame > 340 && videoSection.frame < 400) {
+            $('.sc-character .txt3').addClass('ani-translate')
+        } else {
+            $('.sc-character .txt3').removeClass('ani-translate')
+        }
+        
+    },
+    toggleClass: {
+        targets: '.sc-character',
+        className: 'active'
     }
 })
 
-images[0].onload = render;
+// images[0].onload = render;
 
-function render() {
-    ctx.drawImage(images[videoSection.frame], 0, 0);
-}
+// function render() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height)
+//     ctx.drawImage(images[videoSection.frame], 0, 0);
+// }
+
+
+gsap.utils.toArray('.sc-fact .group-title .title').forEach((title, idx) => {
+    ScrollTrigger.create({
+        trigger: title.parentNode,
+        start: 'top bottom',
+        end: 'bottom bottom',
+        animation: gsap.from(title, { opacity: 0, yPercent: 100, rotateX: -90, duration: 1, delay: 0.1, stagger: { each: 0.1, ease: 'power4.in'}}),
+    })
+})
+
+const infoWidth = gsap.getProperty('.sc-fact .info-item', 'width');
+const infoTl = gsap.timeline()
+.to('.sc-fact .info-item', { x: -infoWidth * 7, repeat: -1, duration: 60 })
+
+ScrollTrigger.create({
+    trigger: '.sc-fact',
+    start: 'top top',
+    end: 'bottom bottom',
+    onUpdate: (self) => {
+        let factor = 2.5;
+        
+        if (self.direction < 0) {
+            factor *= -1;
+        }
+        
+        gsap.timeline()
+        .to(infoTl, { timeScale: factor * 2.5, duration:  0.2})
+        .to(infoTl, { timeScale: factor / 2.5, duration: 1}, "+=0.3")
+    },
+    scrub: 0,
+})
+
+ScrollTrigger.create({
+    trigger: '.sc-fact .group-info',
+    start: 'top bottom',
+    end: 'bottom bottom',
+    scrub: 0,
+    // once: true,
+    markers:true,
+    onEnter: () => {
+        $('.sc-fact .group-info').addClass('is-ani');
+    }
+})
+
