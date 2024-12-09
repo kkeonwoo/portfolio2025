aa79d08afc63639e1be3f4b8a327e39eca7c0a02
kkeonwoo <kunwoo1324@naver.com>
2024-12-09 13:36:12 +0900

    alf - jazean

--- jazean/assets/js/common.js
+++ jazean/assets/js/common.js
@@ -1,32 +1,46 @@
 Jazean = {
     init: function () {
-        gsap.registerPlugin(ScrollTrigger);
-        gsap.defaults({ease: 'none'});
-
-        // this.openingAni();
+        this.openingAni();
         this.setStrokeAnimation('.path');
-        this.scrollAni();
+        // this.scrollAni();
         this.setSwiper();
+        this.handleCursor();
+        this.handleSnb();
     },
     openingAni: function() {
         $('#wrap').imagesLoaded()
         .done( function( instance ) {
+            const headTxt = new SplitType('.sc-visual .txt-area *', { types: 'chars', });
+
             const opeingTl = gsap.timeline()
             .set('body', { onStart: () => {
                 $('body').addClass('is-loaded');
             }})
             .to('.line-logo .path', { strokeDashoffset: 0})
             .to('.line-logo .logo-roots', { autoAlpha: 1})
-            .from('.sc-visual .txt-area .char', { autoAlpha: 0, yPercent: 100, ease: 'linear', stagger: { amount: 2, ease: 'power2.in'}})
-            .from('#header .link', { autoAlpha: 0, yPercent: -100, stagger: { amount: 1.5, from: 'end', ease: 'powe2.out'}})
-            .to('.sc-visual .txt-area', { autoAlpha: 0, yPercent: 100}, '+=1')
+            // .from('.sc-visual .txt-area .char', { autoAlpha: 0, yPercent: 100, ease: 'linear', stagger: { amount: 2, ease: 'power2.in'}})
+            // .from('#header .link', { autoAlpha: 0, yPercent: -100, stagger: { amount: 1.5, from: 'end', ease: 'powe2.out'}})
+            // .to('.sc-visual .txt-area', { autoAlpha: 0, yPercent: 100}, '+=1')
             .to('.group-line', { y: -56}, '<')
-            .from('.snb-item', { autoAlpha: 0, y: 100, stagger: { each: 0.2}})
-            .from('.scroll-circle', { autoAlpha: 0}, '<')
-            .set('body', { overflow: 'auto'})
+            // .from('.snb-item', { autoAlpha: 0, y: 100, stagger: { each: 0.2}})
+            // .from('.scroll-circle', { autoAlpha: 0}, '<')
+            // .set('body', { overflow: 'auto'})
         });
     },
     scrollAni: function() {
+        // const svgTl = gsap.timeline()
+        // .to('.sc-visual .path', { strokeDashoffset: 0})
+        // ScrollTrigger.create({
+        //     trigger: '.sticky-wrap',
+        //     start: 'top top',
+        //     end: 'bottom bottom',
+        //     animation: svgTl,
+        //     scrub: true,
+        //     markers:true,
+        //     onUpdate:() => {
+        //         console.log('aaa');
+        //     }
+        // })
         // gsap.utils.toArray('section').forEach((sc, idx) => {
         //     let path = $(sc).find('.line:not(".line-logo") .path');
             
@@ -62,14 +76,14 @@ Jazean = {
         //     }
         // })
 
-        ScrollTrigger.create({
-            trigger: '.sc-roast',
-            start: 'top top',
-            end: 'bottom bottom',
-            // animation: ,
-            markers: true,
-            scrub: 0,
-        })
+        // ScrollTrigger.create({
+        //     trigger: '.sc-roast',
+        //     start: 'top top',
+        //     end: 'bottom bottom',
+        //     // animation: ,
+        //     markers: true,
+        //     scrub: 0,
+        // })
     },
     setSwiper: function() {
         const swiperProduct = new Swiper('.sc-product .swiper-product', {
@@ -81,7 +95,7 @@ Jazean = {
             navigation: {
                 nextEl: ".swiper-product .btn-next",
                 prevEl: ".swiper-product .btn-prev",
-           },
+            },
         })
 
         const swiperNews = new Swiper('.sc-news .swiper-news', {
@@ -89,7 +103,33 @@ Jazean = {
             navigation: {
                 nextEl: ".sc-news .btn-next",
                 prevEl: ".sc-news .btn-prev",
-           },
+            },
+        })
+    },
+    handleCursor: function() {
+        // quickTo: 성능최적화 유틸리티
+        // transform: 순서대로 적용되는 변환 함수들의 조합.
+        const cursor = $('.cursor');
+        let xTo = gsap.quickTo(cursor, 'x', { duration: 0.4, ease: 'back(3)'})
+        let yTo = gsap.quickTo(cursor, 'y', { duration: 0.4, ease: 'back(3)'})
+
+        gsap.set(cursor, {xPercent: -50, yPercent: -50});
+
+        $(window).on('mousemove', function({ clientX: x, clientY: y}) {
+            xTo(x);
+            yTo(y);
+        })
+    },
+    handleSnb: function() {
+        const $linkSnb = $('.snb .link-snb');
+
+        $linkSnb.each((idx, link) => {
+            $(link).on('click', function(e) {
+                e.preventDefault();
+                let target = $(this).attr('href');
+                
+                lenis.scrollTo(target)
+            })
         })
     },
     getPathLength: function(t) {
@@ -98,6 +138,7 @@ Jazean = {
     setStrokeAnimation: function(t) {
         $(t).each((_, path) => {
             const pathLength = this.getPathLength(path);
+            
             gsap.set(path, { strokeDasharray: pathLength, strokeDashoffset: pathLength})
         })
     }
--- jazean/assets/js/scroll.js
+++ jazean/assets/js/scroll.js
@@ -1,1 +1,167 @@
+$(document).ready(function() {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.defaults({ease: 'none'});
 
+    // line의 타임라인과 개별 섹션의 애니메이션과 잘 안맞는 느낌
+    // line이 너무 빨리 실행됨
+    // 이 경우 tween의 duration을 늘려 scrolltrigger에서 차지하는 비중을 늘려야하는지
+    // dom의 높이를 늘려야하는지 애매함
+    const svgTl = gsap.timeline()
+    .to('.sc-visual .line-gradient .path', { strokeDashoffset: 0})
+    .to('.sc-bean .line-bean .path', { strokeDashoffset: 0})
+    .to('.sc-grain .line-grain .path', { strokeDashoffset: 0})
+    .to('.sc-grain .line-farmer .outline', { strokeDashoffset: 0})
+    .to('.sc-grain .line-farmer .path:not(.outline)', { strokeDashoffset: 0})
+    .to('.sc-tech .line .path', { strokeDashoffset: 0})
+    .to('.sc-roast .top .path', { strokeDashoffset: 0})
+    .to('.sc-roast .line-bean .path', { strokeDashoffset: 0})
+    .to('.sc-roast .bottom .path', { strokeDashoffset: 0})
+    .to('.sc-discover .line .path', { strokeDashoffset: 0})
+    .from('.sc-discover .line .logo', { opacity: 0}, '<')
+    
+    ScrollTrigger.create({
+        trigger: '.sticky-wrap',
+        start: 'top top',
+        end: 'bottom bottom',
+        animation: svgTl,
+        scrub: 0,
+    })
+
+    const beanTween = gsap.from('.sc-bean .txt-area > *, .ico-bean', { autoAlpha: 0, yPercent: 100, stagger: { each: 0.2 }})
+
+    ScrollTrigger.create({
+        trigger: '.sc-bean',
+        start: 'top top',
+        end: 'bottom top',
+        onEnter: () => beanTween,
+    })
+
+    const grainTl = gsap.timeline()
+    .set('.sc-grain .cherry-box', { yPercent: idx => {
+        return (idx + 2) * 100;
+    }})
+    .to('.sc-grain .cherry-box', { yPercent: 0, rotate: idx => {
+        return idx === 0 ? 185 : idx === 1 ? 110 : 202;
+    }})
+    
+    ScrollTrigger.create({
+        trigger: '.sc-grain .inner',
+        start: 'top bottom',
+        end: 'bottom top',
+        animation: grainTl,
+        scrub: 0,
+    })
+
+    const grainTween = gsap.from('.sc-grain .txt-area > *', { autoAlpha: 0, yPercent: 100, stagger: { each: 0.2 }})
+    ScrollTrigger.create({
+        trigger: '.sc-grain',
+        start: 'top top',
+        end: 'bottom top',
+        onEnter: () => grainTween,
+    })
+
+    const techTween = gsap.from('.sc-tech .txt-area > *', { autoAlpha: 0, yPercent: 100, stagger: { each: 0.2 }})
+    ScrollTrigger.create({
+        trigger: '.sc-tech',
+        start: 'top top',
+        end: 'bottom top',
+        onEnter: () => techTween,
+    })
+
+    const techVideo = document.querySelector(".sc-tech video");
+    const techTl = gsap.timeline()
+    .from('.floating-bean', {'--yPercent': 0})
+    // video가 load되고 난 이후 실행해야 druation을 구할 수 있음
+    techVideo.onloadedmetadata = function () {
+        techTl.to(techVideo, { currentTime: techVideo.duration }, '<')
+    };
+
+    ScrollTrigger.create({
+        trigger: '.sc-tech .inner',
+        start: 'top bottom',
+        end: 'bottom top',
+        animation: techTl,
+        // markers:true,
+        scrub: 1, // 비디오가 끊겨보인 이유
+    })
+    
+    const roastVideo = document.querySelector("#flavor video");
+    const roastVideoTl = gsap.timeline()
+    roastVideo.onloadedmetadata = function () {
+        roastVideoTl.to(roastVideo, { currentTime: roastVideo.duration })
+    }
+    ScrollTrigger.create({
+        trigger: '#flavor',
+        start: 'top bottom',
+        end: 'bottom top',
+        animation: roastVideoTl,
+        markers: true,
+        scrub: 1,
+    })
+
+    const roastTxt = new SplitType('.sc-roast .txt-area .title', { types: 'words' });
+    const roastTl = gsap.timeline()
+    .to('.sc-roast', { '--inset': 0, duration: 5})
+    .to('.sc-roast .txt-area1 .title .word', { autoAlpha: 0, yPercent: -100, stagger: { amount: 0.5}}, '<')
+    .to('.sc-roast .txt-area1 .txt', { autoAlpha: 0}, '<')
+    .from('.sc-roast .txt-area2 .title .word', { autoAlpha: 0, yPercent: 100, stagger: { amount: 0.5}}, '-=3.6')
+    .from('.sc-roast .txt-area2 .txt', { autoAlpha: 0}, '-=2.8')
+
+
+    ScrollTrigger.create({
+        trigger: '.sc-roast',
+        start: 'top top',
+        end: 'bottom bottom',
+        animation: roastTl,
+        markers: true,
+        scrub: 0,
+    })
+
+    const discoverTL = gsap.timeline()
+    .from('.sc-discover .txt-area > *, .sc-discover .link', { autoAlpha: 0, yPercent: 100, stagger: { each: 0.2 }})
+    .to('.sc-discover .line .path', { strokeDashoffset: 0}, '<')
+
+    ScrollTrigger.create({
+        trigger: '.sc-discover',
+        start: 'top center',
+        end: 'bottom center',
+        markers:true,
+        onEnter: () => discoverTL,
+    })
+
+    const centerTxt = new SplitType('.sc-center .title-area .title', { types: 'words' });
+    const centerTL = gsap.timeline()
+    .from('.sc-center .title .word, .sc-center .link', { autoAlpha: 0, yPercent: 100, stagger: { amount: 0.3 }})
+    .from('.sc-center .txt', { autoAlpha: 0}, '<')
+
+    ScrollTrigger.create({
+        trigger: '.sc-center',
+        start: 'top top',
+        end: 'bottom top',
+        markers:true,
+        onEnter: () => centerTL,
+    })
+
+    const newsTween = gsap.from('.sc-news .swiper-slide', { autoAlpha: 0, xPercent: 100, ease: 'sine.out', duration: 1, stagger: { each: 0.3 }})
+
+    ScrollTrigger.create({
+        trigger: '.sc-news',
+        start: 'top center',
+        end: 'bottom center',
+        markers:true,
+        onEnter: () => newsTween,
+    })
+
+    const footerTL = gsap.timeline()
+    .from('#footer .ft-top > *', { autoAlpha: 0, xPercent: 100, ease: 'sine.out', stagger: { each: 0.3 }})
+    .from('#footer .ft-bottom *', { autoAlpha: 0, ease: 'sine.out', stagger: { each: 0.3 }})
+
+    ScrollTrigger.create({
+        trigger: '#footer',
+        start: 'top center',
+        end: 'bottom center',
+        markers:true,
+        onEnter: () => footerTL,
+    })
+
+})
